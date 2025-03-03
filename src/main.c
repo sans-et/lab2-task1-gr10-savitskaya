47f17b0e7eb97f3de8440822a250db6a65f27385
Hanna-Maryia Savitskaya <103072354+sans-et@users.noreply.github.com>
2025-03-03 16:31:34 +0000

    Updated main.c and configuration files

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
-/* hello.c */
 #include <stdio.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+void main(void) {
+  printf("Hello World\n");
+  printf("Goodbye World\n");
 }
